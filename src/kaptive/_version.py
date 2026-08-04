49c165803b34453f1e28086f6bdc8b3a8409b0ff
Tom Stanton <tomdstanton@gmail.com>
2026-08-04 18:05:44 +1000

    fix: add missing train extra to test deps and typing to bgc modules

--- src/kaptive/_version.py
+++ src/kaptive/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = "3.3.2.dev0+gb62369b1e.d20260803"
-__version_tuple__ = version_tuple = (3, 3, 2, "dev0", "gb62369b1e.d20260803")
+__version__ = version = '3.3.2.dev5+gcc4c43116.d20260804'
+__version_tuple__ = version_tuple = (3, 3, 2, 'dev5', 'gcc4c43116.d20260804')
 
 __commit_id__ = commit_id = None
